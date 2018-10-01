5ffa9615be453eb9d6f78d7830e222bf44bd7e27
50enta <valtercinquenta@outlook.com>
2018-10-01 00:35:05 -1200

    primeira pedra
    Primeiro commit, ou seja, lançamento da primeira pedra para o  projeto.

---
+++ bissecao/src/model/Calculo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author AN_2018
+ */
+public class Calculo {
+    private double a;
+    private double b;
+    
+    private ArrayList<Object> calculos;
+    
+    private int calcularPontoMedio(){
+        
+        return 0;
+    }
+    
+}
